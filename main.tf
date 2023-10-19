129744803ee7faf03df307a573dc37e6e83d7f28
Aman960007 <amandeep3095@gmail.com>
2023-10-19 16:06:12 +0530

    adding files

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "run_shell_script" {
   provisioner "local-exec" {
-    command = "chmod +x pod.sh && ./pod.sh <aws_access_key> <aws_secret_key>"
+    command = "chmod +x pod.sh && ./pod.sh AKIAXBOVP6PEMWQX6WIC mMHv5rklWru2jhDECxu7av+ps3wKZS2x7I+dt8Tj"
     interpreter = ["/bin/bash", "-c"]
   }
 }
